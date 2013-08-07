907477a0553ba98d00a5b190cf56a1e64f580419
wes hayutin <whayutin@redhat.com>
2013-08-07 11:48:47 -0400

    version.py is hardcoding the version, will now take it from the spec

--- packstack/version.py
+++ packstack/version.py
@@ -1,11 +1,15 @@
+import subprocess
 
-VERSION = ['2013', '2', '1']
+#VERSION = ['2013', '2', '1']
+proc = subprocess.Popen(["cat openstack-packstack.spec | grep Version | awk '{print $2}'"], stdout=subprocess.PIPE, shell=True)
+#print('VERSION= ' + str(VERSION))
+(out, err) = proc.communicate()
+VERSION = out.split(".")
+print("VERSION= " + str(out))
 FINAL=False
 
 def version_string():
     if FINAL:
         return '.'.join(filter(None, VERSION))
     else:
-        return '.'.join(filter(None, VERSION))+"dev"
-        
-
+        return '.'.join(filter(None, VERSION))
